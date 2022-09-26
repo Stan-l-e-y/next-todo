ddc2845c21986249467753698758b8a42192fae8
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-09-26 08:08:53 -0400

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const { data: session, status } = useSession();
-  //test
+  //testi
   return (
     <div className={styles.container}>
       <Head>
